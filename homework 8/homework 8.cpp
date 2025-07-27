025899db779a11e2b2e9b65f5e6c3e5ae10c0b8c
Дмитрий <dmitriy5556788@gmail.com>
2025-07-27 18:22:47 +0300

    develop: дополнительные буквы

--- homework 8/homework 8.cpp
+++ homework 8/homework 8.cpp
@@ -1,16 +1,20 @@
-﻿// homework 8.cpp: определяет точку входа для приложения.
+// homework 8.cpp: определяет точку входа для приложения.
 //
 
 #include "homework 8.h"
 
 int main()
 {
-    setlocale(LC_ALL,"Rus");
+    //setlocale(LC_ALL,"Rus");
+    std::locale::global(std::locale("ru_RU.UTF-8"));
+   
     const int SIZE = 8;
     char array[SIZE][SIZE];
-    char letters[] = {'F', 'E', 'R'};
+    char letters[] = {'F', 'E', 'R', '1', 'Y', '2'};
+    //std::string l[] = { "Щ" };
 
-    for (char letter : letters) 
+
+    for (char letter : letters)
     {
         for (int i = 0; i < SIZE; i++) 
         {
@@ -39,6 +43,23 @@ int main()
                             (j == SIZE-1 && i > 0 && i < SIZE/2) ||
                             (i == j && i > SIZE/2))
                             array[i][j] = 'R';
+                        break;
+                    case '1':// Буква Щ
+                        if ((7 != i && (0 == j || 3 == j || 6 == j)) ||
+                            (6 == i) || (7 == i && 7 == j))
+                            array[i][j] = '1';
+                        break;
+                    case 'Y':
+                        if ((i == j && j < SIZE / 2 || j == (SIZE - 2 - i) && j > i) ||
+                            (j == (SIZE / 2 - 1) && j < i))
+                            array[i][j] = 'Y';
+                        break;
+                    case '2':// Буква Б
+                        if ((0 == i || 0 == j || SIZE - 1 == i && SIZE - 1 != j) ||
+                            SIZE - 4 == i && SIZE - 1 != j || 
+                            SIZE - 1 == j && SIZE - 3 == i ||
+                            SIZE - 1 == j && SIZE - 2 == i)
+                            array[i][j] = '2';
                         break;
                 }
             }
